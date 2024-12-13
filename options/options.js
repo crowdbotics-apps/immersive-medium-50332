e959aa0cb4ee0999f16cce64f9e6e0175adb4f13
Crowdbotics <team@crowdbotics.com>
2024-12-13 04:16:18 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "immersive-medium-50332",
+  url: "https://immersive-medium-50332.botics.co",
+  api: "https://immersive-medium-50332.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
